a83bc4f87b8aa5c985ef94afc5d3b67abaf7b85e
Jacob Yaw <yawjacob@yahoo.com>
2022-02-23 18:24:38 -0600

    adding basic functionality to components

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Comments from "./Comments";
+import Profile from "./Profile";
+import Home from "./Home";
+import NavBar from "./NavBar";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <NavBar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/profile">
+                    <Profile />
+                </Route>
+                <Route exact path="/comments">
+                    <Comments />
+                </Route>
+            </Switch>
+        </div>
+    )
+}
+
+export default App
