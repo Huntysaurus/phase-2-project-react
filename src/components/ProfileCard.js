8c120f41c48e8bedd9861431aaba165b59c463e1
Jacob Yaw <yawjacob@yahoo.com>
2022-02-24 20:26:13 -0600

    profiles appearing in home, adding like button

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ProfileCard() {
+function ProfileCard({ profiles }) {
+
+    const [like, setLike] = useState(false)
+
+    console.log(profiles)
     return (
-        <p>profile card</p>
+        profiles.map(profile => {
+            return (
+            <div key={profile.id}>
+                <h3>{profile.name}</h3>
+                <p>{profile.description}</p>
+                <p>{profile.interests}</p>
+                <img src={profile.photo} alt={profile.name}></img>
+                <button onClick={() => setLike(true)}>Like</button>
+            </div>
+            )
+        })
     )
 }
 
