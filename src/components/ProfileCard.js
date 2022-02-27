588175ace495e0dcf007c3fe1221a8ca686cbcc2
Jacob Yaw <yawjacob@yahoo.com>
2022-02-27 17:10:16 -0600

    in the process of figuring out how to get if/else statement to work for profile/form

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,33 +3,44 @@ import ProfileForm from "./ProfileForm";
 function ProfileCard() {
 
 
-    const [userProfile, setUserProfile] = useState("")
+    const [userProfile, setUserProfile] = useState([{
+        id:0,
+        name:"",
+        description:"",
+        interests:"",
+        photo:""
+    }
+    ])
 
-    useEffect(()=> {
-        fetch("http://localhost:3000")
-        .then(res => res.json())
-        .then(data => handleUserProfile(data))
-    }, [])
+    // useEffect(()=> {
+    //     fetch("http://localhost:3000")
+    //     .then(res => res.json())
+    //     .then(data => handleUserProfile(data))
+    // }, [])
 
 
         function handleUserProfile(profileObj) {
             setUserProfile(profileObj)
             console.log(userProfile)
         }
-
+            if (userProfile.includes(userProfile.id > 0)) {
             return (
-                <div>
-                    <ProfileForm onProfileFormSubmit={handleUserProfile} />
                     <div key={userProfile.id}>
                         <h3>{userProfile.name}</h3>
                         <p>{userProfile.description}</p>
                         <p>{userProfile.interests}</p>
                         <img src={userProfile.photo} alt={userProfile.name}></img>
+                        <button>edit profile</button>
+                        <button>delete profile</button>
                     </div>
-                </div>
 
             )
+            } else {
+                   return (
+                 <ProfileForm onProfileFormSubmit={handleUserProfile} />
+            )
         }
+    }
 
 //individual profile cards get created in here and displayed inside the Home component
 
