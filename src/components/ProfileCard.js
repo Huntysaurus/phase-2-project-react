1be7034e90bc6f9c2b459a64de8d9db6372b43e6
Jacob Yaw <yawjacob@yahoo.com>
2022-02-28 21:01:06 -0600

    trying to get if/else profile functionality to work

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,48 +1,53 @@
 import React, { useState, useEffect } from "react";
 import ProfileForm from "./ProfileForm";
+
+
+
 function ProfileCard() {
 
+    const [userProfile, setUserProfile] = useState(null)
 
-    const [userProfile, setUserProfile] = useState([{
-        id:0,
-        name:"",
-        description:"",
-        interests:"",
-        photo:""
-    }
-    ])
+    console.log(userProfile)
 
-    // useEffect(()=> {
-    //     fetch("http://localhost:3000")
-    //     .then(res => res.json())
-    //     .then(data => handleUserProfile(data))
-    // }, [])
+    useEffect(()=> {
+        fetch("http://localhost:4000/profileData")
+        .then(res => res.json())
+        .then(data => setUserProfile(data))
+    }, [])
 
 
-        function handleUserProfile(profileObj) {
-            console.log(profileObj)
-            setUserProfile(profileObj)
-            console.log(userProfile)
+    function handleUserProfile(profileObj) {
+        console.log(profileObj)
+        fetch("http://localhost:4000/profileData", {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(profileObj)
+        })
+        .then(res => res.json())
+        .then(data => setUserProfile(data))
         }
-            if (userProfile.includes(userProfile.id > 0)) {
-            return (
-                    <div key={userProfile.id}>
-                        <h3>{userProfile.name}</h3>
-                        <p>{userProfile.description}</p>
-                        <p>{userProfile.interests}</p>
-                        <img src={userProfile.photo} alt={userProfile.name}></img>
-                        <button>edit profile</button>
-                        <button>delete profile</button>
-                    </div>
 
-            )
-            } else {
-                   return (
-                 <ProfileForm onProfileFormSubmit={handleUserProfile} />
+        if (userProfile === []) {
+        return (
+            <ProfileForm onProfileFormSubmit={handleUserProfile} />
+                
+        )
+        } else {
+                return (
+                    <div key={userProfile[0].id}>
+                    <h3>{userProfile[0].name}</h3>
+                    <p>{userProfile[0].description}</p>
+                    <p>{userProfile[0].interests}</p>
+                    <img src={userProfile[0].photo} alt={userProfile[0].name}></img>
+                    <button>edit profile</button>
+                    <button>delete profile</button>
+                </div>
             )
         }
     }
 
-//individual profile cards get created in here and displayed inside the Home component
+//profile card or form depending on if created or not
 
 export default ProfileCard
